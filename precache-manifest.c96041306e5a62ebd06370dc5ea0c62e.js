695e0f1582f3a25181194a68b41af78f28eef4d1
Sujan362 <u5918127@au.edu>
2020-08-14 19:12:52 +0700

    Updates

---
+++ precache-manifest.c96041306e5a62ebd06370dc5ea0c62e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc37af3e5fdf28fc5d1e26328abe5929",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "550bf82587238c334280",
+    "url": "/todo/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "c40f241264244c853ebc",
+    "url": "/todo/static/js/2.e0596216.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo/static/js/2.e0596216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "550bf82587238c334280",
+    "url": "/todo/static/js/main.562b39f9.chunk.js"
+  },
+  {
+    "revision": "9b0b323eb4e95e94d05a",
+    "url": "/todo/static/js/runtime-main.8c1f14b9.js"
+  }
+]);
